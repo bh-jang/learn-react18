ff56ed521728d5bad36c7c392e96107d14ffd0a7
hoony.jang <hoony.jang@goodoc.co.kr>
2023-08-20 22:03:21 +0900

    test: automatic betch

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const handleAutomaticBetchingClick = () => {
+    setCount(pre => pre + 1);
+    setTimeout(() => {
+      setCount(pre => pre + 1);
+      setCount(pre => pre + 1);
+    }, 1000)
+    setTimeout(() => {
+      setCount(pre => pre + 1);
+      setCount(pre => pre + 1);
+    }, 1000)
+  }
+
+  useEffect(() => {
+    if (count > 0) {
+      console.log('render check log')
+    }
+  })
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div style={{ marginBottom: '32px' }}>
+          {count}
+        </div>
+        <button onClick={handleAutomaticBetchingClick}>Automatic Betching</button>
       </header>
     </div>
   );
